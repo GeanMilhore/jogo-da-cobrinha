033a6924c03f77b667392e4d8bbb270c096a13ff
GeanMilhore <gean.milhore@gmail.com>
2021-03-26 06:22:16 -0300

    Adiciona direções em looping

--- Code/script.js
+++ Code/script.js
@@ -9,19 +9,39 @@ snake[0] = {
 let direction = "right"
 
 function criarBG(){
-  context.fillStyle = "lightgreen"
+  context.fillStyle = "black"
   context.fillRect(0, 0, 16 * box, 16 * box) // desenha o nosso retangulo
 }
 
 // A cobrinha vai se comportar como um array de coordenadas
 function criarCobrinha(){
   for (i = 0; i<snake.length; i++){
-    context.fillStyle = "green"
+    context.fillStyle = "lightgray"
     context.fillRect(snake[i].x, snake[i].y, box, box)
   }
 }
 
+// criando um evento de escuta
+document.addEventListener('keydown',update)
+
+function update (event){
+  if (event.keyCode == 37 && direction != 'right') direction = 'left'
+  if (event.keyCode == 40 && direction != 'down') direction = 'up'
+  if (event.keyCode == 39 && direction != 'left') direction = 'right'
+  if (event.keyCode == 38 && direction != 'up') direction = 'down'
+  
+}
+
+
 function iniciarJogo(){
+
+// atravessar paredes
+  if(snake[0].x > 15 * box && direction == 'right') snake[0].x = 0
+  if(snake[0].x < 0  && direction == 'left') snake[0].x = 16 * box
+  if(snake[0].y > 15 * box && direction == 'up') snake[0].y = 0 
+  if(snake[0].y < 0  && direction == 'down') snake[0].y = 16 * box
+
+
   criarBG()
   criarCobrinha()
 
@@ -35,7 +55,7 @@ function iniciarJogo(){
     snakeX -= box
   }
   if ( direction == "up"){
-    snakeY =+ box
+    snakeY += box
   }
   if ( direction == "down"){
     snakeY -= box
